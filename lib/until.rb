01a9b522d5acef83664d201cccb49a5e47a6b77c
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-20 17:18:39 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,10 +2,10 @@ def using_until
   
   levitation_force = 6
 
-until levitation_force == 10
-  
-  puts "Wingardium Leviosa"
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
     levitation_force += 1
-    
+  end
+
 end
 
